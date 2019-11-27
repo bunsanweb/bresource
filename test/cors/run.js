84bac8646d884e0d71f2bc1602c6c69c6b7e73a8
bellbind <bellbind@gmail.com>
2019-11-27 14:52:53 +0900

    [update] remove commonjs require completely
    - required: node >= 13.2.0

--- test/cors/run.js
+++ test/cors/run.js
@@ -1,35 +1,17 @@
-const path = require("path");
+import * as runner from "../runner.js";
 
-const LocalWebServer = require("local-web-server");
-const puppeteer = require("puppeteer");
-
-const port = 9000, port1 = 9100, port2 = 9200;
-const ws = LocalWebServer.create({port, directory: __dirname});
-const ws1 = LocalWebServer.create(
-  {port: port1, directory: path.resolve(__dirname, "h1/")});
-const ws2 = LocalWebServer.create(
-  {port: port2, directory: path.resolve(__dirname, "h2/")});
-
-(async () => {
-  const browser = await puppeteer.launch({
+runner.run([
+  [import.meta.url, 9000],
+  [new URL("./h1/", import.meta.url).href, 9100],
+  [new URL("./h2/", import.meta.url).href, 9200],
+], {
+  launch: {
     //headless: false, appMode: true, devtools: true,
     //args: ["--disable-web-security"],
-  });
-  try {
-    const page = await browser.newPage();
-    page.on("error", err => console.error(err));
-    page.on("pageerror", err => console.error(err));
-    page.on("console", msg => {
-      const {url, lineNumber, columnNumber} = msg.location();
-      const fmt = `[${url} ${lineNumber}:${columnNumber}]: ${msg.text()}`;
-      if (msg.type() === "assert") console.assert(false, fmt);
-      else console[msg.type()](fmt);
-    });
-    await page.goto(`http://localhost:${port}`, {waitUntil: "networkidle0"});
-  } finally {
-    await browser.close();
-    ws.server.close();
-    ws1.server.close();
-    ws2.server.close();
-  }
-})().catch(console.error);
+  }, 
+  goto: {
+    waitUntil: "networkidle0",
+    // timeout: 300 * 1000, 
+  },
+  //timeout: 300 * 1000,
+}).catch(console.error);
--- test/fetch/run.js
+++ test/fetch/run.js
@@ -1,29 +1,15 @@
-const path = require("path");
+import * as runner from "../runner.js";
 
-const LocalWebServer = require("local-web-server");
-const puppeteer = require("puppeteer");
-
-const port = 9000;
-const ws = LocalWebServer.create({port, directory: __dirname,});
-
-(async () => {
-  const browser = await puppeteer.launch({
+runner.run([
+  [import.meta.url, 9000],
+], {
+  launch: {
     //headless: false, appMode: true, devtools: true,
     //args: ["--disable-web-security"],
-  });
-  try {
-    const page = await browser.newPage();
-    page.on("error", err => console.error(err));
-    page.on("pageerror", err => console.error(err));
-    page.on("console", msg => {
-      const {url, lineNumber, columnNumber} = msg.location();
-      const fmt = `[${url} ${lineNumber}:${columnNumber}]: ${msg.text()}`;
-      if (msg.type() === "assert") console.assert(false, fmt);
-      else console[msg.type()](fmt);
-    });
-    await page.goto(`http://localhost:${port}`, {waitUntil: "networkidle0"});
-  } finally {
-    await browser.close();
-    ws.server.close();
-  }
-})().catch(console.error);
+  }, 
+  goto: {
+    waitUntil: "networkidle0",
+    // timeout: 300 * 1000, 
+  },
+  //timeout: 300 * 1000,
+}).catch(console.error);
